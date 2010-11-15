49ac3fef17b64e4cda88c036527e72037bc214a9
Jérôme Wax <jerome.wax@lo2k.net>
2010-11-15 17:29:21 +0100

    add geocode

--- src/net/lo2k/where/GetPosition.java
+++ src/net/lo2k/where/GetPosition.java
@@ -1,10 +1,17 @@
 package net.lo2k.where;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
@@ -86,11 +93,33 @@ public class GetPosition extends MapActivity {
 	protected void makeUseOfNewLocation(Location location) {
 		//TextView text = (TextView) findViewById(R.id.gpsCoord);
 		MapView map = (MapView) findViewById(R.id.mapview);
-		map.setClickable(true);
+		//map.setClickable(true);
 		map.setBuiltInZoomControls(true);
 		GeoPoint p = new GeoPoint((int)(location.getLatitude()*1E6), (int)(location.getLongitude()*1E6));
 		MapController mc = map.getController();
 		mc.animateTo(p);
+		
+		//geocode !
+		Geocoder geoCoder = new Geocoder(
+                getBaseContext(), Locale.getDefault());
+		
+		try {
+            List<Address> addresses = geoCoder.getFromLocation(
+                p.getLatitudeE6()  / 1E6, 
+                p.getLongitudeE6() / 1E6, 1);
+
+            String add = "";
+            if (addresses.size() > 0) 
+            {
+                for (int i=0; i<addresses.get(0).getMaxAddressLineIndex(); 
+                     i++)
+                   add += addresses.get(0).getAddressLine(i) + "\n";
+            }
+
+            Toast.makeText(getBaseContext(), add, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {                
+            e.printStackTrace();
+        } 
 		//map.a
 		//map.
 		//text.setText(location.toString());
