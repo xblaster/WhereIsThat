5b0a44a3fac914fd7db1e3483cd93a1b043f52e9
Jérôme Wax <jerome.wax@lo2k.net>
2010-11-15 17:04:28 +0100

    add map

--- src/net/lo2k/where/GetPosition.java
+++ src/net/lo2k/where/GetPosition.java
@@ -1,23 +1,53 @@
 package net.lo2k.where;
 
-import android.app.Activity;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class GetPosition extends Activity {
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class GetPosition extends MapActivity {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_pos);
         
-        Button btaction = (Button) findViewById(R.id.takeNewLoc);
+      //define locationManager
+		final LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		final LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location
+				// provider.
+				makeUseOfNewLocation(location);
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			public void onProviderEnabled(String provider) {
+			}
+
+			public void onProviderDisabled(String provider) {
+			}
+		};
+		
+		locationManager.requestLocationUpdates(
+				LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+	}
+	
+	@Override
+	protected boolean isRouteDisplayed() {
+	    return false;
+	}
+/*        
+        //Button btaction = (Button) findViewById(R.id.takeNewLoc);
         
         //define locationManager
 		final LocationManager locationManager = (LocationManager) this
@@ -43,7 +73,7 @@ public class GetPosition extends Activity {
 		locationManager.requestLocationUpdates(
 				LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
 		
-		btaction.setOnClickListener(new OnClickListener() {
+		/*btaction.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
@@ -52,9 +82,16 @@ public class GetPosition extends Activity {
 			}
 		});
     }
-
+*/
 	protected void makeUseOfNewLocation(Location location) {
-		TextView text = (TextView) findViewById(R.id.gpsCoord);
-		text.setText(location.toString());
+		//TextView text = (TextView) findViewById(R.id.gpsCoord);
+		MapView map = (MapView) findViewById(R.id.mapview);
+		
+		GeoPoint p = new GeoPoint((int)location.getLatitude(), (int)location.getLongitude());
+		MapController mc = map.getController();
+		mc.animateTo(p);
+		//map.a
+		//map.
+		//text.setText(location.toString());
 	}
 }
